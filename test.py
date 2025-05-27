a72e9709bdcfb9cd6effdda339e7e8ab0a8c7099
Bravudo <gcursino.souza@gmail.com>
2025-05-27 00:07:46 -0300

    Sistema de Gerenciamento de Missões

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ import os
 def clean():
      os.system('cls' if os.name == 'nt' else 'clear')
 def finish_clean():
-    time.sleep(5)
+    time.sleep(3)
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+carteira = { 'ouro': 0}
 missoes = [ { 'nome': 'Mundo Corrompido', 'dificuldade': 'Impossível', 'status': 'Pendente','recompensa': 50},
 { 'nome': 'Planeta 02', 'dificuldade': 'facil', 'status': 'Inativo','recompensa': 10},
 { 'nome': 'Tornado Infernal', 'dificuldade': 'Difícil', 'status': 'Inativo','recompensa': 25} ]
@@ -18,13 +19,13 @@ MinhasMissoes = []
 
 dado = 0
 
-menuNames = ['List Quest', 'Accept Quest', 'Finish Quest', 'Fail Quest', 'Leave'] 
+menuNames = ['My Quest', 'Accept Quest', 'Finish Quest', 'Fail Quest', 'Ver Carteira', 'Leave'] 
 menu = 0
 def system():
     global menu
     clean()  
     while True:
-        menu = int(input(f'<><>MENU<><>\n1-{menuNames[0]}\n2-{menuNames[1]}\n3-{menuNames[2]}\n4-{menuNames[3]}\n5-{menuNames[4]}\n<Option><-->'))
+        menu = int(input(f'<><>MENU<><>\n1-{menuNames[0]}\n2-{menuNames[1]}\n3-{menuNames[2]}\n4-{menuNames[3]}\n5-{menuNames[4]}\n6-{menuNames[5]}\n<Option><-->'))
 
         #Listar Todas Missões
         if menu == 1:
@@ -45,7 +46,7 @@ def system():
                     if missao['status'] == 'Pendente' or missao['status'] == 'Inativo':
                         print(f'{indice + 1} - Missão: {missao['nome']} <> Dificuldade: {missao['dificuldade']}')
                         print()
-            select = int(input(f'Escolha alguma opção:'))
+            select = int(input(f'Escolha alguma opção: '))
             if select <= len(missoes):
                  indice = select - 1
                  quest = missoes.pop(indice)
@@ -53,8 +54,39 @@ def system():
                  MinhasMissoes.append(quest)
                  print(f'--> Missão Selecionada: {quest['nome']} - {quest['dificuldade']} - ${quest['recompensa']}')
             finish_clean()  
+        
 
+        if menu == 3:
+            clean()   
+            for indice, missao in enumerate(MinhasMissoes):
+                if missao['status'] == 'Andamento':
+                    print(f'{indice + 1} - Missão: {missao['nome']} <> Dificuldade: {missao['dificuldade']} <$> Recompensa: {missao['recompensa']}')
+                    print()
+            select = int(input('Escolha alguma opção: '))
+            
+            if 1 <= select <= len(MinhasMissoes): 
+                indice = select - 1
+                carteira['ouro'] += MinhasMissoes[indice]['recompensa']
+                print(f'Missão {MinhasMissoes[indice]['nome']} Finalizada')
+                del MinhasMissoes[indice]
+                finish_clean()  
+            else:
+                print('Opção Invalida')
+                finish_clean()  
+
+        if menu == 4:
+            for indice, missao in enumerate(MinhasMissoes):
+                print(f'{indice + 1}- {missao['nome']}')
+            select = int(input('Escolha um numero para deletar: '))
+            print(f'Falha na missão do {MinhasMissoes[indice]['nome']}')
+            del MinhasMissoes[indice - 1]
+            finish_clean()  
         if menu == 5:
+            clean() 
+            print(f'---Inventário---\nOuro {carteira['ouro']}🪙')  
+            finish_clean()    
+
+        if menu == 6:
             print('--Finalizado--')
             break
 
