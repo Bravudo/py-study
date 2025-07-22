17792999206660448cab817c4eeceda181864dd9
Bravudo <gcursino.souza@gmail.com>
2025-07-22 11:53:52 -0300

    fix: Nomenclatura das variaveis

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-p = [{ 'nome': 'cleiton', 'invI': 10000, 'pontuação': 6, 'horatrabalhada': 233, 'div': 0 },{ 'nome': 'jorge', 'invI': 2322, 'pontuação': 10, 'horatrabalhada': 233, 'div': 0}]
-#50% do lucro será repassado aos sócios
+#Projeto Para melhora de lógica matematica
+
+p = [{ 'nome': 'cleiton', 'invI': 10000, 'PontosReunião': 6, 'horatrabalhada': 233, 'div': 0 },{ 'nome': 'jorge', 'invI': 2322, 'PontosReunião': 10, 'horatrabalhada': 233, 'div': 0}]
 empresa = {'nome':'MadeInChina', 'lucro': 30000}
 
 
@@ -11,7 +12,7 @@ def repasseDeLucros():
 
     for socio in p:
         investimentoInicial = socio['invI']
-        pontos = socio['pontuação']
+        pontos = socio['PontosReunião']
         horas = socio['horatrabalhada']
 
         totalInvestimento += investimentoInicial
@@ -29,7 +30,7 @@ def repasseDeLucros():
     for socio in p:
         nome = socio['nome']
         investimentoInicial = socio['invI']
-        pontos = socio['pontuação']
+        pontos = socio['PontosReunião']
         horas = socio['horatrabalhada']
         pontuacaofinal = socio['div']
         
@@ -43,15 +44,9 @@ def repasseDeLucros():
         pontuacaofinal = spHoras + spPontos + spInvestimento 
         pontuacaofinal = pontuacaofinal / 3 # 3 é a quantidade de medidas que eu usei no calculo
 
-        lucroRepartido = empresa['lucro'] / 100
+        lucroRepartido = (empresa['lucro'] * 0.5)  / 100
         lucroRepartido = lucroRepartido * pontuacaofinal
 
-        print(f'----\n{socio['nome']}\nInvestimento: {spInvestimento:.2f}%\nPontuação Média:{spPontos:.2f}%\nHoras: {spHoras:.2f}%\nDireito a lucro: {pontuacaofinal:.2f}%\nGanho: R${lucroRepartido:.2f}')
+        print(f'----\n{socio['nome']}\nInvestimento: {spInvestimento:.1f}%\nPontuação Média: {spPontos:.0f}%\nHoras: {spHoras:.0f}%\nDireito a lucro: {pontuacaofinal:.1f}%\nGanho: R${lucroRepartido:.2f}')
     
-        
-
-
-
-
-
 repasseDeLucros()
