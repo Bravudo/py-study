a7d8bc04fa5f8c9a392b42c68b72630915b85ea0
Bravudo <gcursino.souza@gmail.com>
2025-07-23 18:14:44 -0300

    Divisão entre dinheiro e quantidade de escolhas

--- investimento.py
+++ investimento.py
@@ -15,16 +15,16 @@ DivisaoDeAcoesNaCarteira = 6 # Quantidade de Ações diferentes em sua carteira
 
 # Completar A lista proporcionalmente em quanto você quer que cada area da sua carteira tenha de risco
 # Risco: 0 a 10 - Completar até o total de 10
-AtivoRiscoAlto = 1 # Risco 7 ao 10
-AtivoRiscoMedio = 0 # Risco 3 ao 6
-AtivoRiscoBaixo = 4 # Risco 0 ao 2
+AtivoRiscoAlto = 5 # Risco 7 ao 10
+AtivoRiscoMedio = 2 # Risco 3 ao 6
+AtivoRiscoBaixo = 3 # Risco 0 ao 2
 
 print(f'Saldo para investimento: ${DinheiroParaInvestir:.2f}')
 
 #O Retorno significa quanto mensalmente ela te devolve em porcentagem.
 ação = [
     #ativos agressivos
-    {'nome': 'PALANTIR TECHNOLOGIES INC', 'code': 'PLTR', 'preco': 148.7, 'risco': 7, 'tempo': TempoInvestimento, 'retorno': 13},
+    {'nome': 'PALANTIR TECHNOLOGIES INC', 'code': 'PLTR', 'preco': 148.7, 'risco': 8, 'tempo': TempoInvestimento, 'retorno': 13},
     {'nome': 'NVIDIA CORPORATION', 'code': 'NVDA', 'preco': 167.8, 'risco': 7, 'tempo': TempoInvestimento, 'retorno': 9},
     {'nome': 'JORGES MEDIA TV', 'code': 'JMT', 'preco': 17.8, 'risco': 10, 'tempo': TempoInvestimento, 'retorno': 14},
     #dividendos passivos
@@ -36,15 +36,31 @@ ação = [
 def MaquinaDeEscolha():
     dinheiro = DinheiroParaInvestir
 
+    DinheiroAlto = (dinheiro / 100) * (AtivoRiscoAlto * 10)
+    DinheiroMedio = (dinheiro / 100) * (AtivoRiscoMedio * 10)
+    DinheiroBaixo = (dinheiro / 100) * (AtivoRiscoBaixo * 10)
+
+    print(int(DinheiroAlto))
+
     #Quanto  em porcentagem comprar de cada ativo
-    qtdAtivoPerigo = (DivisaoDeAcoesNaCarteira/10) * (AtivoRiscoAlto)
-    qtdAtivoMediano = (DivisaoDeAcoesNaCarteira/10) * (AtivoRiscoMedio) 
-    qtdAtivoPassivo = (DivisaoDeAcoesNaCarteira/10) * (AtivoRiscoBaixo) 
+    qtdAtivoAlto = (DivisaoDeAcoesNaCarteira/10) * (AtivoRiscoAlto)
+    qtdAtivoMedio = (DivisaoDeAcoesNaCarteira/10) * (AtivoRiscoMedio) 
+    qtdAtivoBaixo = (DivisaoDeAcoesNaCarteira/10) * (AtivoRiscoBaixo) 
 
-    print(f'Ativos\n Alto Risco: {qtdAtivoPerigo:.1f}\n Médio Risco: {qtdAtivoMediano:.1f} Baixo Risco: {qtdAtivoPassivo:.1f}')
+    EscolhaDeAcoes(qtdAtivoAlto, DinheiroAlto)
 
 
-    
+
+    print(f'Quantidade de Ativos\n Alto Risco: {qtdAtivoAlto:.1f}\n Médio Risco: {qtdAtivoMedio:.1f}\n Baixo Risco: {qtdAtivoBaixo:.1f}')
+
+
+
+
+def EscolhaDeAcoes(qtdAtivo, qtdDinheiro):
+    Nome = ação[0]['nome']
+    print(Nome)
+
+
 
 MaquinaDeEscolha()
 
